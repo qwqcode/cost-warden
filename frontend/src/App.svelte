2d8cc6f10963bdb18d71e28e661f4a914c30dad9
qwqcode <qwqcode@gmail.com>
2022-05-29 14:09:19 +0800

    feat: Set up frontend project
    Signed-off-by: qwqcode <qwqcode@gmail.com>

---
+++ frontend/src/App.svelte
@@ -0,0 +1,56 @@
+<script>
+import Login from "./Login.svelte";
+
+export let name;
+</script>
+
+<main>
+	<div class="app">
+		<div class="brand"></div>
+		<div class="body">
+			<Login />
+		</div>
+	</div>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+	}
+
+	.app {
+		display: flex;
+		flex-direction: row;
+		width: 80vw;
+		height: 90vh;
+		background: #fff;
+	}
+
+	.brand {
+		display: flex;
+		flex-basis: 30%;
+		position: relative;
+		background: linear-gradient(180deg, #008DDD 0%, #00A3FF 100%);
+		height: 100%;
+	}
+
+	.brand:after {
+		position: absolute;
+		content: '';
+		background-image: url(/logo.svg);
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: 60%;
+		height: 100%;
+		width: 100%;
+	}
+	
+	.body {
+		display: flex;
+		flex: 1;
+	}
+</style>
---
+++ frontend/src/Login.svelte
@@ -0,0 +1,60 @@
+<script>
+
+</script>
+
+<div class="login">
+    <input type="text" placeholder="用户名" autocomplete="off">
+    <input type="password" placeholder="密码">
+    <div class="bottom">
+        <button type="submit">登录</button>
+        <button class="signup-btn">注册</button>
+    </div>
+</div>
+
+<style>
+    .login {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    input {
+        width: 270px;
+        line-height: 37px;
+        padding: 0 20px;
+        background: #F1F1F1;
+        border: 0;
+        border-left: 5px solid transparent;
+        outline: none;
+    }
+
+    input:focus {
+        border-left-color: #00A3FF;
+    }
+
+    .bottom {
+        margin-top: 20px;
+        width: 270px;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    button {
+        cursor: pointer;
+        display: inline-block;
+        width: 80px;
+        height: 37px;
+        border: 0;
+        background: #00A3FF;
+        color: #fff;
+    }
+
+    .signup-btn {
+        cursor: pointer;
+        background: transparent;
+        color: #000;
+        width: fit-content;
+    }
+</style>
